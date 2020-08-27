e70467bb3f0191759c4314db9b1a12cd44965543
Nick Quinn <nick@nickquinn.co.uk>
2020-08-27 19:11:19 +0100

    Use constants for actions

--- index.js
+++ index.js
@@ -1,79 +1,4 @@
-
-{
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: "Learn Redux",
-    complete: false
-  }
-}
-
-{
-  type: "REMOVE_TODO",
-  id: 0
-}
-
-{
-  type: "TOGGLE_TODO",
-  id: 0
-}
-
-{
-  type: "ADD_GOAL",
-  goal: {
-    id: 0,
-    name: "Run a Marathon"
-  }
-}
-
-{
-  type: "REMOVE_GOAL"
-  id: 0
-}
-
-/*
-Characteristics of a Pure function
-1) They always return the same result if the same arguments are passed in.
-2) They depend only on the arguments passed into them.
-3) Never produce any side effects.
-*/
-
-//Reducer function
-function todos(state = [], action) {
-  switch(action.type) {
-    case "ADD_TODO":
-      return state.concat([action.todo])
-    case "REMOVE_TODO":
-      return state.filter((todo) => todo.id !== action.id)
-    case "TOGGLE_TODO":
-      return state.map((todo) => todo.id !== action.id ? todo : 
-      //Object.assign merges instead of creating a whole new object and
-      //manually cloning it
-        Object.assign({}, todo, {complete: !todo.complete})
-      )
-    default: 
-      return state
-  }
-}
-
-function goals(state = [], action) {
-  switch(action.type) {
-    case "ADD_GOAL":
-      return state.concat([action.goal])
-    case "REMOVE_GOAL":
-      return state.filter((goal) => goal.id !== action.id)
-    default: 
-      return state
-  }
-}
-
-function app(state = {}, action) {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action)
-  }
-}
-
+//LIBRARY CODE
 function createStore(reducer) {
   //The store should have four parts:
   // 1. The state.
@@ -95,78 +20,131 @@ function createStore(reducer) {
 
   const dispatch = (action) => {
     // Call todos
-    state = reducer(state, action)
+    state = reducer(state, action);
     // loop over all listeners and invoke them
-    listeners.forEach((listener) => listener())
-  }
+    listeners.forEach((listener) => listener());
+  };
 
   return {
     getState,
     subscribe,
-    dispatch
+    dispatch,
   };
 }
 
-const store = createStore(app)
+/*
+Characteristics of a Pure function
+1) They always return the same result if the same arguments are passed in.
+2) They depend only on the arguments passed into them.
+3) Never produce any side effects.
+*/
+
+//APP CODE
+
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+//Reducer function
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : //Object.assign merges instead of creating a whole new object and
+            //manually cloning it
+            Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+
+const store = createStore(app);
 
 store.subscribe(() => {
-  console.log('The new state is: ', store.getState())
-})
+  console.log("The new state is: ", store.getState());
+});
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: "ADD_TODO",
   todo: {
     id: 0,
-    name: 'Walk the dog',
+    name: "Walk the dog",
     complete: false,
-  }
-})
+  },
+});
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 1,
-    name: 'Wash the car',
+    name: "Wash the car",
     complete: false,
-  }
-})
+  },
+});
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 2,
-    name: 'Go to the gym',
+    name: "Go to the gym",
     complete: true,
-  }
-})
+  },
+});
 
 store.dispatch({
-  type: 'REMOVE_TODO',
-  id: 1
-})
+  type: REMOVE_TODO,
+  id: 1,
+});
 
 store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 0
-})
+  type: TOGGLE_TODO,
+  id: 0,
+});
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 0,
-    name: 'Learn Redux'
-  }
-})
+    name: "Learn Redux",
+  },
+});
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 1,
-    name: 'Lose 20 pounds'
-  }
-})
+    name: "Lose 20 pounds",
+  },
+});
 
 store.dispatch({
-  type: 'REMOVE_GOAL',
-  id: 0
-}) 
+  type: REMOVE_GOAL,
+  id: 0,
+});
